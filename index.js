db0ef23b453b7486147e26985240baee34c9837d
Joseph <josephshonko99@gmail.com>
2024-04-04 08:31:46 +0300

    commiting dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+  
+//   element.append(ul);
+
+
+  
+const body1 = document.getElementById("main");
+if (body1) {
+  body1.parentNode.removeChild(body1);
+}
+
+const newHeader = document.createElement("h1"); 
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion"; 
+document.body.appendChild(newHeader);
+  
+  
+
